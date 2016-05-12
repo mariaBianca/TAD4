76a53a26943e04a15cdf355da00f869aa13ec209
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2016-05-12 12:43:02 +0200

    le changes

--- Explosion.java
+++ Explosion.java
@@ -5,15 +5,8 @@ public class Explosion extends SpaceObject {
 
 	SpaceObject explosion = new SpaceObject();
 
-	public void createExplosion(){
-
-
-
-	}
-
 	public void initExplosions() {
 
-<<<<<<< HEAD
 		int i;
 
 		for (i = 0; i < AsteroidGame.MAX_SCRAP; i++) {
@@ -120,72 +113,6 @@ public class Explosion extends SpaceObject {
 			// AsteroidGame.explosionCounter = AsteroidGame.SCRAP_COUNT;
 		}
 	}
-=======
-	   
-
-	    
-	      explosion.shape = new Polygon();
-	      explosion.active = false;
-	    
-
-	  }
-
-	  public void explode(Explosion s) {
-
-	    int c, i, j;
-	    int cx, cy;
-
-	    // Create sprites for explosion animation. The each individual line segment
-	    // of the given sObj is used to create a new sObj that will move
-	    // outward  from the sObj's original position with a random rotation.
-
-	    s.render();
-	    c = 2;
-	    if (AsteroidGame.detail || s.sObj.npoints < 6)
-	      c = 1;
-	    for (i = 0; i < s.sObj.npoints; i += c) {
-	      AsteroidGame.explosionIndex++;
-	      if (AsteroidGame.explosionIndex >= AsteroidGame.MAX_SCRAP)
-	        AsteroidGame.explosionIndex = 0;
-	      explosion.active = true;
-	      explosion.shape = new Polygon();
-	      j = i + 1;
-	      if (j >= s.sObj.npoints)
-	        j -= s.sObj.npoints;
-	      cx = (int) ((s.shape.xpoints[i] + s.shape.xpoints[j]) / 2);
-	      cy = (int) ((s.shape.ypoints[i] + s.shape.ypoints[j]) / 2);
-	      explosion.shape.addPoint(
-	        s.shape.xpoints[i] - cx,
-	        s.shape.ypoints[i] - cy);
-	      explosion.shape.addPoint(
-	        s.shape.xpoints[j] - cx,
-	        s.shape.ypoints[j] - cy);
-	      explosion.x = s.x + cx;
-	      explosion.y = s.y + cy;
-	      explosion.angle = s.angle;
-	      explosion.deltaAngle = 4 * (Math.random() * 2 * AsteroidGame.MAX_ROCK_SPIN - AsteroidGame.MAX_ROCK_SPIN);
-	      explosion.deltaX = (Math.random() * 2 * AsteroidGame.MAX_ROCK_SPEED - AsteroidGame.MAX_ROCK_SPEED + s.deltaX) / 2;
-	      explosion.deltaY = (Math.random() * 2 * AsteroidGame.MAX_ROCK_SPEED - AsteroidGame.MAX_ROCK_SPEED + s.deltaY) / 2;
-	     // AsteroidGame.explosionCounter = AsteroidGame.SCRAP_COUNT;
-	    }
-	  }
-
-	  public void updateExplosions() {
-
-	    int i;
-
-	    // Move any active explosion debris. Stop explosion when its counter has
-	    // expired.
-
-	    for (i = 0; i < AsteroidGame.MAX_SCRAP; i++)
-	      if (explosion.active) {
-	        explosion.advance();
-	        explosion.render();
-	        if (--AsteroidGame.explosionCounter[i] < 0)
-	          explosion.active = false;
-	      }
-	  }
->>>>>>> e929581dc911aea34b11c8de3274f5c83632913a
 
 }
 
